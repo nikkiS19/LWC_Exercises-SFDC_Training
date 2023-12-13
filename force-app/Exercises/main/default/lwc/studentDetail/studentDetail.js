9308dd98abd08a7685bbf4613efc208287257482
nikki-sharma <Nikki.Sharma@mheducation.com>
2023-12-13 13:55:06 +0530

    Exercise 2-10 Completed

--- force-app/Exercises/main/default/lwc/studentDetail/studentDetail.js
+++ force-app/Exercises/main/default/lwc/studentDetail/studentDetail.js
@@ -7,21 +7,35 @@ import { getRecord,getFieldValue,getFieldDisplayValue } from 'lightning/uiRecord
 import DESCRIPTION from '@salesforce/schema/Contact.Description';
 import EMAIL from '@salesforce/schema/Contact.Email';
 import PHONE from '@salesforce/schema/Contact.Phone';
-
+import { subscribe, unsubscribe, MessageContext } from 'lightning/messageService';
 import FIELD_Name from '@salesforce/schema/Contact.Name';
 const fields = [FIELD_Name,DESCRIPTION,EMAIL,PHONE];
-
+import SELECTED_STUDENT_CHANNEL from '@salesforce/messageChannel/SelectedStudentChannel__c';
 export default class StudentDetail extends LightningElement {
 
 	// TODO #3: locate a valid Contact ID in your scratch org and store it in the studentId property.
 	// Example: studentId = '003S000001SBAXEIA5';
-	studentId = '0037d00002DQIwVAAX';
-
+	studentId = '';
+	subscription;
+	@wire(MessageContext) messageContext;
 	//TODO #4: use wire service to call getRecord, passing in our studentId and array of fields.
 	//		   Store the result in a property named wiredStudent.
 	@wire(getRecord, { recordId :'$studentId', fields })
 	wiredStudent;
-		
+	
+	connectedCallback() {
+		if(this.subscription){
+		return;
+		}
+		this.subscription = subscribe(
+		this.messageContext,
+		SELECTED_STUDENT_CHANNEL,
+		(message) => {
+		this.handleStudentChange(message)
+		}
+		);
+		}
+
 	get name() {
 		return this._getDisplayValue(this.wiredStudent.data, FIELD_Name);
 	}
@@ -52,5 +66,12 @@ export default class StudentDetail extends LightningElement {
 	_getDisplayValue(data, field) {
 		return getFieldDisplayValue(data, field) ? getFieldDisplayValue(data, field) : getFieldValue(data, field);
 	}
+	handleStudentChange(message) {
+		this.studentId = message.studentId;
+		}
+		disconnectedCallback() {
+			unsubscribe(this.subscription);
+			this.subscription = null;
+			}	
 	
 }
