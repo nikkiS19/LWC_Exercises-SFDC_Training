4dd46148131f8b7981b671a982f3a69e8cce440f
nikki-sharma <Nikki.Sharma@mheducation.com>
2023-12-14 16:29:05 +0530

    Exercise 5-1 Completed

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -2,11 +2,33 @@ import { LightningElement, wire } from 'lwc';
 import getStudents from '@salesforce/apex/StudentBrowser.getStudents';
 import { publish, MessageContext } from 'lightning/messageService';
 import SELECTED_STUDENT_CHANNEL from '@salesforce/messageChannel/SelectedStudentChannel__c';
-export default class StudentBrowser extends LightningElement {
+import { NavigationMixin } from 'lightning/navigation';
+export default class StudentBrowser extends NavigationMixin(LightningElement) {
     selectedDeliveryId = '';
     selectedInstructorId = '';
     @wire(getStudents, { instructorId: '$selectedInstructorId', courseDeliveryId: '$selectedDeliveryId' })
     students;
+    cols = [
+        {
+            fieldName: "Name",
+            label: "Name"
+        },
+        {
+            fieldName: "Title",
+            label: "Title",
+            hiddenOnMobile: true
+        },
+        {
+            fieldName: "Phone",
+            label: "Phone",
+            type: "phone"
+        },
+        {
+            fieldName: "Email",
+            label: "E-Mail",
+            type: "email"
+        }
+    ];
     @wire(MessageContext) messageContext;
     handleFilterChange(event) {
         this.selectedDeliveryId = event.detail.deliveryId;
@@ -19,6 +41,17 @@ export default class StudentBrowser extends LightningElement {
     updateSelectedStudent(studentId) {
         publish(this.messageContext, SELECTED_STUDENT_CHANNEL, {
             studentId: studentId
+        });
+    }
+    handleRowDblClick(event) {
+        const studentId = event.detail.pk;
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: studentId,
+                objectApiName: 'Contact',
+                actionName: 'edit'
+            }
         });
     }
 }
