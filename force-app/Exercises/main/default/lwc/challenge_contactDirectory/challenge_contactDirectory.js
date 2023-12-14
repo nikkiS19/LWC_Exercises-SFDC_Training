cdf2e44f915661fac3a839e57ca3d9617544fd37
nikki-sharma <Nikki.Sharma@mheducation.com>
2023-12-14 14:22:59 +0530

    extra credit challenge 1

---
+++ force-app/Exercises/main/default/lwc/challenge_contactDirectory/challenge_contactDirectory.js
@@ -0,0 +1,13 @@
+import { LightningElement,wire } from 'lwc';
+//import getContacts from '@salesforce/apex/Contacts.getContacts'; 
+export default class Challenge_contactDirectory extends LightningElement {
+    error;
+    /*@wire(getContacts,{firstLetter :''})
+    getContactsbyFilter({error,data}){
+        if(error){
+            error = this.error;
+        }else{
+
+        }
+    }*/
+}
