36ba0e0227296c8f6342596291181e5e568b690b
nikki-sharma <Nikki.Sharma@mheducation.com>
2023-12-15 13:26:36 +0530

    extra credit challenge 1.2

--- force-app/Exercises/main/default/lwc/challenge_contactDirectory/challenge_contactDirectory.js
+++ force-app/Exercises/main/default/lwc/challenge_contactDirectory/challenge_contactDirectory.js
@@ -1,13 +1,30 @@
 import { LightningElement,wire } from 'lwc';
-//import getContacts from '@salesforce/apex/Contacts.getContacts'; 
+import getContacts from '@salesforce/apex/Contacts.getContacts'; 
 export default class Challenge_contactDirectory extends LightningElement {
-    error;
-    /*@wire(getContacts,{firstLetter :''})
-    getContactsbyFilter({error,data}){
-        if(error){
-            error = this.error;
-        }else{
-
+    error; contactId;contactsFiltered;
+    letters = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+    columnConfig = [
+        {
+            label: 'Name',
+            fieldName: 'name',
+            type: 'text'
+        }, {
+            label: 'Email',
+            fieldName: 'email',
+            type: 'email'
+        }, {
+            label: 'Phone',
+            fieldName: 'phone',
+            type: 'phone'
         }
-    }*/
-}
+    ];
+    @wire(getContacts,{firstLetter :''})
+    getContactsbyFilter(result){
+            this.contactsFiltered = [];
+            if(result.data){
+                this.contactsFiltered = result.data.map(); 
+            }else if (result.error) {
+                this.error = result.error;
+            } 
+        } 
+    }
