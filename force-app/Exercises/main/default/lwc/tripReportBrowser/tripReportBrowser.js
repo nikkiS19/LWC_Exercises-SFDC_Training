3e979ed2be2ac5ebc1967883282b2f35721da723
nikki-sharma <Nikki.Sharma@mheducation.com>
2023-12-15 18:56:54 +0530

    Exercise 6-4 Edit Record

--- force-app/Exercises/main/default/lwc/tripReportBrowser/tripReportBrowser.js
+++ force-app/Exercises/main/default/lwc/tripReportBrowser/tripReportBrowser.js
@@ -62,4 +62,10 @@ export default class TripReportBrowser extends LightningElement {
 	onBtnNewClick() {
 		this.changeTripReportMode('add');
 	}
+	handleRowClick(event) {
+		this.selectedRecordId = event.detail.pk;
+	}
+	handleRowDblClick() {
+		this.changeTripReportMode('edit');
+	}
 }
