619e58530637c16bb598dd737e53c7777d32e07a
nikki-sharma <Nikki.Sharma@mheducation.com>
2023-12-18 15:25:56 +0530

    extra credit 1 completed

--- EXFiles/Challenges/challenge_contactDirectory/challenge_contactDirectory.js
+++ EXFiles/Challenges/challenge_contactDirectory/challenge_contactDirectory.js
@@ -5,7 +5,6 @@ import getContacts from '@salesforce/apex/Contacts.getContacts';
 export default class Challenge_contactList extends LightningElement {
 
 	firstLetter = ''; 
-	@wire(getContacts, {firstLetter: '$firstLetter'}) contacts;
 	letters = ('a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z').toUpperCase().split(',');	
 
 	//this same task could be accomplished using imperative Apex in the constructor.
@@ -27,6 +26,8 @@ export default class Challenge_contactList extends LightningElement {
 			type: 'phone'
 		}
 	];
+	
+	@wire(getContacts, {firstLetter: '$firstLetter'}) contacts;
 
 	onLetterSelect(event) { 
 		this.firstLetter = event.detail.value;
